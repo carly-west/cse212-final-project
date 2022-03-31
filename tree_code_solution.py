87e0fa42e2b0efec3ab606f060ebae810799b4f8
carly-west <82841070+carly-west@users.noreply.github.com>
2022-03-30 20:01:15 -0600

    Create tree_code_solution.py

---
+++ tree_code_solution.py
@@ -0,0 +1,39 @@
+# Here is how to instatiate a Binary Search Tree
+class Node:
+    def __init__(self, value=None):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def insertNode(self, value):
+
+        if not self.value:
+            self.value = value
+            return
+
+        if self.value == value:
+            return
+
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+                return
+            self.left = Node(value)
+            return
+
+        if self.right:
+            self.right.insert(value)
+            return
+        self.right = Node(value)
+
+    def deleteNode(self, value, root):
+        if not root:
+            return root
+
+        if root.value > value:
+            root.left = deleteNode(self, value, root.left)
+
+        elif root.value < value:
+            root.right = deleteNode(self, value, root.right)
+
+        # still working on this.....
